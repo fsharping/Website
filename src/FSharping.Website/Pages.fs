27cbbb765b46b57eb0efe2ea3e0db2ac5513a596
Roman Provazník <dzoukr@dzoukr.cz>
2016-01-31 21:34:31 +0100

    less redesign

--- src/FSharping.Website/Pages.fs
+++ src/FSharping.Website/Pages.fs
@@ -5,13 +5,13 @@ open Shaver.Razor
 
 let home : WebPart = 
     [
-        ("Menu", partial "views/menu.html" "dashboard");
+        ("Menu", partial "views/menu.html" "home");
         ("Content", partial "views/main.html" null);
     ] |> masterPage "views/layout.html" null
 
 let error404 : WebPart =
     [
-        ("Menu", partial "views/menu.html" "dashboard");
+        ("Menu", partial "views/menu.html" "home");
         ("Content", partial "views/404.html" null);
     ] |> masterPageWithCode HTTP_404 "views/layout.html" null
 
